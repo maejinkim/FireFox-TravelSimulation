d959eb786658246099f30308d2c1c1c4aad419e1
김혜진 <sikhye823@naver.com>
2019-11-08 15:50:39 +0900

    upload

---
+++ travelSimulation/app/src/main/java/com/example/travelsimulation/App.java
@@ -0,0 +1,69 @@
+package com.example.travelsimulation;
+
+import android.app.Application;
+import android.os.StrictMode;
+
+public class App extends Application {
+
+    private String name;
+    private int gender;
+    private int stamina;
+    private int hungry;
+    private int money;
+    private int joy;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+        StrictMode.setVmPolicy(builder.build());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getGender() {
+        return gender;
+    }
+
+    public void setGender(int gender) {
+        this.gender = gender;
+    }
+
+    public int getStamina() {
+        return stamina;
+    }
+
+    public void setStamina(int stamina) {
+        this.stamina = stamina;
+    }
+
+    public int getHungry() {
+        return hungry;
+    }
+
+    public void setHungry(int hungry) {
+        this.hungry = hungry;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+
+    public int getJoy() {
+        return joy;
+    }
+
+    public void setJoy(int joy) {
+        this.joy = joy;
+    }
+}
---
+++ travelSimulation/app/src/main/java/com/example/travelsimulation/SplashActivity.java
@@ -0,0 +1,36 @@
+package com.example.travelsimulation;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new splashHandler(), 1000);
+
+
+    }
+
+    private class splashHandler implements Runnable{
+        @Override
+        public void run() {
+            startActivity(new Intent(getApplication(), MainActivity.class));
+            SplashActivity.this.finish();
+        }
+    }
+
+
+    @Override
+    public void onBackPressed() {
+
+    }
+}
